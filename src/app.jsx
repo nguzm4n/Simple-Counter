a1297c90676e58bd0baa91adef8925734e15c606
Nicolás Guzmán <nguzmanespinoza1@gmail.com>
2024-03-27 18:02:53 -0300

    casi terminado

--- src/app.jsx
+++ src/app.jsx
@@ -5,27 +5,31 @@ import { useEffect, useState } from 'react'
 
 const App = () => {
 
-  const [numero, setNumero] = useState(0)
+const [numero, setNumero] = useState(0)
+ 
 
 useEffect(() => {
-  
   const interval = setInterval(() => {
-    setNumero(numero + 1)
+    setNumero(valorActual => valorActual + 1)
   }, 1000)
 return () => clearInterval(interval)
-})
+}, [])
 
 
 
   return (
     <div>
         <div className='container '>
-        <Contador/>       
-        <Contador/>       
-        <Contador/>       
-        <Contador/>       
         <Contador
-          numero={numero} />     
+        numero={Math.floor(numero/10000%10)}/>       
+        <Contador
+        numero={Math.floor(numero/1000%10)}/>       
+        <Contador
+        numero={Math.floor(numero/100%10)}/>       
+        <Contador
+       numero={Math.floor(numero/10%10)}/>       
+        <Contador
+          numero={Math.floor(numero/1%10)} />     
           
         </div>
     </div>
